2653ac90f415febcc57d85a029d266e93fedb784
berdyshevv <viktorberdysh@gmail.com>
2019-12-25 22:35:01 +0200

    done 1.1.6

--- 1.1 Basics/1.1.6.c
+++ 1.1 Basics/1.1.6.c
@@ -6,3 +6,16 @@
 
 * Например: ввод: 4, 6 ⇒ вывод: 16, 36, 52
 */
+#include <stdio.h>
+
+int main(void){
+    int one;
+    int two;
+    scanf("%d%d", &one, &two);
+    printf("%d times %d equals %d\n%d times %d equals %d\n%d plus %d equals %d", 
+    one, one, one * one, 
+    two, two, two * two, 
+    one * one, two * two, one * one + two * two);
+
+    return 0;
+}
