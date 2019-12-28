b80e4802520e266abe92b13f7ca59145f43bb35d
berdyshevol <berdyshevo@gmaol.com>
2019-12-28 18:04:18 +0200

    1.2.1.6

--- 1.2. Ветвления/1.2.1. Неполное ветвление/1.2.1.6.c
+++ 1.2. Ветвления/1.2.1. Неполное ветвление/1.2.1.6.c
@@ -10,3 +10,18 @@
  * ввод: 3, 5 ⇒ вывод: дробь положительная; 
  * ввод: -4, 7 ⇒ вывод: дробь отрицательная
  */
+
+#include <stdio.h>
+
+int main(){
+    int n1;
+    int n2;
+    scanf("%d%d", &n1, &n2);
+    if (n1 < 0 && n2 < 0 || n1 > 0 && n2 > 0) {
+        printf("positive\n");
+    }
+    else {
+        printf("negative\n");
+    }
+}
+    
