d979796796e432a08e36a9a0e41a4613b8f07366
berdyshevv <viktorberdysh@gmail.com>
2020-01-01 20:19:49 +0200

    done 1.2.2.1

--- 1.2. Ветвления/1.2.2. Полное ветвление (1)/1.2.2.1.c
+++ 1.2. Ветвления/1.2.2. Полное ветвление (1)/1.2.2.1.c
@@ -8,4 +8,21 @@
  * Например: ввод: 6 ⇒ вывод: положительное число; 
  * ввод: -8 ⇒ вывод: не положительное число; ввод: 0⇒ вывод: не положительное число
  */
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    if (a >0 ) {
+        printf("positive");
+    }
+    else if (a <0 ){
+        printf("negative");    
+    }
+    else{
+        printf("not positive, nor negaive, it is a zero");
+    }
+    
+    return 0;
+}
 
